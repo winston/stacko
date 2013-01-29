665df9715b0c9de14b474f26ab8d02dd175861bf
Winston <winston.yongwei@gmail.com>
2013-01-30 00:10:56 +0800

    Fix failing spec. Rspec subjects are :sob:

--- lib/stacko/utility.rb
+++ lib/stacko/utility.rb
@@ -4,7 +4,7 @@ module Stacko
     def validate_config_keys(config, required_keys)
       raise 'Empty config provided' if config.nil?
       
-      missing_required_keys = required_keys.select { |k| config[k].nil? || config[k].empty? }
+      missing_required_keys = required_keys.select { |key| config[key].nil? || config[key].empty? }
       raise "Missing required config attributes: #{missing_required_keys}" unless missing_required_keys.empty?
       
       config
--- spec/stacko/configuration_spec.rb
+++ spec/stacko/configuration_spec.rb
@@ -1,40 +1,41 @@
 require 'spec_helper'
 
 describe Stacko::Configuration do
-
   let(:environment) { 'production' }
+  
   subject(:standalone_config) { Stacko::Configuration.new "#{File.dirname(__FILE__)}/../fixtures/stacko.standalone.yml", environment }
 
   its(:type) { should == "standalone" }
 
   describe "#validate_yaml" do
-
-    subject(:config_validate_yaml) { standalone_config.validate_config_keys config_hash, required_keys }
+    let(:config_validate_yaml) { standalone_config.validate_config_keys config_hash, required_keys }
 
     context 'when all the required keys are provided' do
-
       let(:config_hash) { {'key1' => 'key1', 'key2' => 'key2', 'key3' => 'key3'} }
       let(:required_keys) { %w{key1 key2} }
 
-      it { expect { config_validate_yaml }.to_not raise_error }
+      it { expect { config_validate_yaml }.to_not raise_error  } 
     end
+    
+    context 'raises an error when config is blank' do
+      let(:config_hash) { nil }
+      let(:required_keys) {  %w{key1} }
 
-    context 'raises an error when any of the required keys are not provided' do
+      it { expect { config_validate_yaml }.to raise_error("Empty config provided") }
+    end      
 
+    context 'raises an error when any of the required keys is not provided' do
       let(:config_hash) { {'key1' => 'key1', 'key3' => 'key3'} }
       let(:required_keys) {  %w{key1 key2} }
 
-      it { expect { config_validate_yaml }.to raise_error }
-
+      it { expect { config_validate_yaml }.to raise_error('Missing required config attributes: ["key2"]') }
     end
 
-    context 'raises an error if the required configuration is empty' do
-
+    context 'raises an error when any of the required keys has an empty value' do
       let(:config_hash) { {'key1' => 'key1', 'key2' => '', 'key3' => 'key3'} }
       let(:required_keys) {  %w{key1 key2} }
 
-      it { expect { config_validate_yaml }.to raise_error }
+      it { expect { config_validate_yaml }.to raise_error('Missing required config attributes: ["key2"]') }
     end
-
   end
 end
